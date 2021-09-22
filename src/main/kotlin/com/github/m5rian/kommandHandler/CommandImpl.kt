d2e96276b8c9f3346473a33a4bfc6643c3ac94e2
Marian <toni_kukoc@gmx.de>
2021-09-22 21:27:52 +0200

    :rotating_light:  Suppressed useless warning

--- src/main/kotlin/com/github/m5rian/kommandHandler/CommandImpl.kt
+++ src/main/kotlin/com/github/m5rian/kommandHandler/CommandImpl.kt
@@ -2,4 +2,5 @@ package com.github.m5rian.kommandHandler
 
 import kotlin.reflect.KFunction
 
+@Suppress("ArrayInDataClass")
 internal data class CommandImpl(val name: String, val aliases: Array<String>, val method: KFunction<*>)
