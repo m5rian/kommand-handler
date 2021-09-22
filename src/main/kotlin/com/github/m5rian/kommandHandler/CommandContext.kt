00b9ac5d25365b90d26635a90fc49c491849d585
Marian <toni_kukoc@gmx.de>
2021-09-22 20:07:08 +0200

    📁 Added TextChannel to CommandContext

--- src/main/kotlin/com/github/m5rian/kommandHandler/CommandContext.kt
+++ src/main/kotlin/com/github/m5rian/kommandHandler/CommandContext.kt
@@ -3,6 +3,7 @@ package com.github.m5rian.kommandHandler
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.Member
+import net.dv8tion.jda.api.entities.TextChannel
 import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 
@@ -10,6 +11,9 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 class CommandContext(val event: GuildMessageReceivedEvent, val member: Member, val executor: String) {
     val bot: JDA = event.jda
     val botMember: Member = event.guild.selfMember
+
     val guild: Guild = event.guild
+    val channel: TextChannel = event.channel
+
     val user: User = event.author
 }
