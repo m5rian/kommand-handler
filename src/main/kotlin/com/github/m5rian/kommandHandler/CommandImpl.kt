c7ccac6f566172e051a4ef59371b6f8437012d79
Marian <toni_kukoc@gmx.de>
2021-09-22 21:26:52 +0200

    :fire:  Removed CommandImpl#description

--- src/main/kotlin/com/github/m5rian/kommandHandler/CommandImpl.kt
+++ src/main/kotlin/com/github/m5rian/kommandHandler/CommandImpl.kt
@@ -2,4 +2,4 @@ package com.github.m5rian.kommandHandler
 
 import kotlin.reflect.KFunction
 
-internal data class CommandImpl(val name: String, val aliases: Array<String>, val description: String, val method: KFunction<*>)
+internal data class CommandImpl(val name: String, val aliases: Array<String>, val method: KFunction<*>)
