1de6728fa2509adc11911d5d6b119cda42abd7de
Marian <toni_kukoc@gmx.de>
2021-09-22 19:54:06 +0200

    👤 Added user to the CommandContext

--- src/main/kotlin/com/github/m5rian/kommandHandler/CommandContext.kt
+++ src/main/kotlin/com/github/m5rian/kommandHandler/CommandContext.kt
@@ -3,6 +3,7 @@ package com.github.m5rian.kommandHandler
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.Member
+import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 
 @Suppress("unused")
@@ -10,4 +11,5 @@ class CommandContext(val event: GuildMessageReceivedEvent, val member: Member, v
     val bot: JDA = event.jda
     val botMember: Member = event.guild.selfMember
     val guild: Guild = event.guild
+    val user: User = event.author
 }
