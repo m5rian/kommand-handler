5b6f1041c0e987886e5242104caeda3a71947e8e
Marian <toni_kukoc@gmx.de>
2021-10-20 12:32:01 +0200

    :fire: Removed Arg#_value!!

--- src/main/kotlin/com/github/m5rian/kommandHandler/resolvers/Arg.kt
+++ src/main/kotlin/com/github/m5rian/kommandHandler/resolvers/Arg.kt
@@ -4,19 +4,17 @@ import com.github.m5rian.kommandHandler.CommandContext
 import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.entities.User
 
-data class Arg<T>(internal val _value: T?, val found: Boolean, val given: Boolean, internal val ctx: CommandContext) {
-    val value: T get() = _value!!
-
+data class Arg<T>(val value: T?, val found: Boolean, val given: Boolean, internal val ctx: CommandContext) {
     companion object {
         fun <T> ofNullable(value: T?, ctx: CommandContext): Arg<T> {
-            return if (value == null) Arg(_value = value, found = false, given = true, ctx = ctx)
-            else Arg(_value = value, found = true, given = true, ctx = ctx)
+            return if (value == null) Arg(value = value, found = false, given = true, ctx = ctx)
+            else Arg(value = value, found = true, given = true, ctx = ctx)
         }
 
-        fun <T> ofNotFound(ctx: CommandContext): Arg<T> = Arg(_value = null, found = false, given = true, ctx = ctx)
-        fun <T> ofNotGiven(ctx: CommandContext): Arg<T> = Arg(_value = null, found = false, given = false, ctx = ctx)
+        fun <T> ofNotFound(ctx: CommandContext): Arg<T> = Arg(value = null, found = false, given = true, ctx = ctx)
+        fun <T> ofNotGiven(ctx: CommandContext): Arg<T> = Arg(value = null, found = false, given = false, ctx = ctx)
     }
 }
 
-fun Arg<User>.getOrDefault(): User = this._value ?: this.ctx.user
-fun Arg<Member>.getOrDefault(): Member = this._value ?: this.ctx.member
+fun Arg<User>.getOrDefault(): User = this.value ?: this.ctx.user
+fun Arg<Member>.getOrDefault(): Member = this.value ?: this.ctx.member
